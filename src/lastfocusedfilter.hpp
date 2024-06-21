9ad2bac683329ab320ad9ac05a06def9c384fb13
falbru <falk.bruskeland@gmail.com>
2024-06-21 23:54:33 +0200

    Add keybindings for focusing in all directions

---
+++ src/lastfocusedfilter.hpp
@@ -0,0 +1,29 @@
+#ifndef LASTFOCUSEDFILTER_HPP
+#define LASTFOCUSEDFILTER_HPP
+
+#include "kakounewidget.hpp"
+#include <QObject>
+#include <QEvent>
+#include <QWidget>
+
+class LastFocusedFilter : public QObject {
+    Q_OBJECT
+
+public:
+    LastFocusedFilter(QObject *parent = nullptr);
+
+    size_t getLastTimeFocused(KakouneWidget* kak_widget) const;
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
+signals:
+    void lastFocusedChanged(QWidget *widget);
+
+private:
+    int m_counter;
+    QMap<KakouneWidget*, size_t> m_last_focused_widgets;
+};
+
+
+#endif
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,8 +2,11 @@
 #include "container.hpp"
 #include "kakounewidget.hpp"
 #include "keybindings.hpp"
+#include "lastfocusedfilter.hpp"
 #include <climits>
+#include <qkeysequence.h>
 #include <qnamespace.h>
+#include <qpainter.h>
 #include <quuid.h>
 #include <qwidget.h>
 
@@ -11,6 +14,8 @@ MainWindow::MainWindow(QString session_id, QWidget *parent) : QMainWindow(parent
 {
     m_id = QUuid::createUuid();
     resize(1024, 768);
+
+    m_last_focused_filter = new LastFocusedFilter(this);
 
     m_draw_options = new DrawOptions();
     m_draw_options->setFont("monospace", 11);
@@ -74,18 +79,88 @@ void MainWindow::focusWindow(const QString &client_name)
     kak_widget->setFocus();
 }
 
+void MainWindow::focusInDirection(std::function<bool(const QRect&, const QRect&)> isWithinBounds)
+{
+    QWidget* focused_widget = focusWidget();
+    QPointF focused_widget_pos = focused_widget->mapToGlobal(focused_widget->rect().topLeft());
+
+    QList<KakouneWidget*> focus_candidates;
+
+    for (KakouneWidget* kak_widget : m_windows) {
+        if (kak_widget) {
+            QPoint kak_widget_pos = kak_widget->mapToGlobal(kak_widget->rect().topLeft());
+
+            QRect kak_widget_rect(kak_widget_pos.x(), kak_widget_pos.y(), kak_widget->width(), kak_widget->height());
+            QRect focused_widget_rect(focused_widget_pos.x(), focused_widget_pos.y(), focused_widget->width(), focused_widget->height());
+
+            if (isWithinBounds(focused_widget_rect, kak_widget_rect)) {
+                focus_candidates.append(kak_widget);
+            }
+        }
+    }
+
+    KakouneWidget* last_focused_candidate = nullptr;
+    int max_last_focused = INT_MIN;
+
+    for (auto& kak_widget : focus_candidates) {
+        int last_focused = m_last_focused_filter->getLastTimeFocused(kak_widget);
+        if (last_focused > max_last_focused) {
+            last_focused_candidate = kak_widget;
+            max_last_focused = last_focused;
+        }
+    }
+
+    if (last_focused_candidate) {
+        last_focused_candidate->setFocus();
+    }
+}
+
+const qreal WIDGET_DIFF_TOLERANCE = 25;
+
 void MainWindow::focusLeft()
 {
-    // TODO
+    focusInDirection(
+        [](const QRect& focused_widget, const QRect& other_widget) {
+            return abs(other_widget.right() - focused_widget.left()) < WIDGET_DIFF_TOLERANCE &&
+                   other_widget.top() <= focused_widget.bottom() &&
+                   focused_widget.top() < other_widget.bottom();
+        }
+    );
 }
 
 void MainWindow::focusRight()
 {
-    // TODO
-    // Container: have a lastFocusedWidget() method
-    // Iterate up to closest container that is Horizontal. Focus lastFocusedWidget
-    // When a Container gains focus, focus the last focused widget
+    focusInDirection(
+        [](const QRect& focused_widget, const QRect& other_widget) {
+            return abs(other_widget.left() - focused_widget.right()) < WIDGET_DIFF_TOLERANCE &&
+                   other_widget.top() <= focused_widget.bottom() &&
+                   focused_widget.top() < other_widget.bottom();
+        }
+    );
 }
+
+void MainWindow::focusUp()
+{
+    focusInDirection(
+        [](const QRect& focused_widget, const QRect& other_widget) {
+            return abs(other_widget.bottom() - focused_widget.top()) < WIDGET_DIFF_TOLERANCE &&
+                   other_widget.left() <= focused_widget.right() &&
+                   focused_widget.left() < other_widget.right();
+        }
+    );
+}
+
+void MainWindow::focusDown()
+{
+    focusInDirection(
+        [](const QRect& focused_widget, const QRect& other_widget) {
+            return abs(other_widget.top() - focused_widget.bottom()) < WIDGET_DIFF_TOLERANCE &&
+                   other_widget.left() <= focused_widget.right() &&
+                   focused_widget.left() < other_widget.right();
+        }
+    );
+}
+
 
 void MainWindow::updateWindowTitle()
 {
@@ -115,6 +190,7 @@ KakouneWidget *MainWindow::createKakouneWidget(const QString &arguments)
     KakouneWidget *kakwidget = new KakouneWidget(m_session->getSessionId(), m_id, m_draw_options, arguments, m_root);
 
     kakwidget->installEventFilter(new KeyBindingsFilter(this));
+    installLastFocusedFilter(kakwidget);
 
     connect(kakwidget, &KakouneWidget::finished, m_root, [=]() {
         m_windows.removeOne(kakwidget);
@@ -129,4 +205,16 @@ KakouneWidget *MainWindow::createKakouneWidget(const QString &arguments)
     m_windows.append(kakwidget);
 
     return kakwidget;
+}
+
+void MainWindow::installLastFocusedFilter(QWidget* widget)
+{
+    widget->installEventFilter(m_last_focused_filter);
+    const QObjectList &children = widget->children();
+    for (QObject *child : children) {
+        QWidget *childWidget = qobject_cast<QWidget *>(child);
+        if (childWidget) {
+            installLastFocusedFilter(childWidget);
+        }
+    }
 }
