0b15032a3ca0bc53d3b8c803a550c7832a695f74
Falk Elvedal Bruskeland <66320400+falbru@users.noreply.github.com>
2024-11-15 15:02:54 +0100

    Fix splits leaving empty gap after being deleted (#78)

--- src/splitcontainer.cpp
+++ src/splitcontainer.cpp
@@ -6,31 +6,39 @@ SplitContainer::SplitContainer(Qt::Orientation orientation, QWidget *parent) : C
 
 void SplitContainer::split(QWidget *source_widget, QWidget *new_widget, Qt::Orientation orientation)
 {
-    disconnectWidget(source_widget);
-
-    int index = m_splitter->indexOf(source_widget);
-
-    SplitContainer *container = new SplitContainer(orientation, this);
-
-    // NOTE: replaceWidget() will move focus to another widget, which will screw up the focus order in
-    // LastFocusedFilter. We therefore clear the focus before calling the function, and reapply the focus after.
-    QWidget *regain_focus_widget = nullptr;
-    if (source_widget->isAncestorOf(focusWidget()))
+    if (m_splitter->count() <= 1)
     {
-        regain_focus_widget = focusWidget();
-        regain_focus_widget->clearFocus();
+        addWidget(new_widget);
+        m_splitter->setOrientation(orientation);
     }
+    else
+    {
+        disconnectWidget(source_widget);
 
-    m_splitter->replaceWidget(index, container);
+        int index = m_splitter->indexOf(source_widget);
 
-    if (regain_focus_widget)
-        regain_focus_widget->setFocus();
+        SplitContainer *container = new SplitContainer(orientation, this);
 
-    // NOTE: Adding the source_widget to the container after calling replaceWidget() will ensure that the splitter ratio
-    // will be preserved.
-    container->addWidget(source_widget);
-    container->addWidget(new_widget);
-    container->m_splitter->setSizes(QList<int>({INT_MAX, INT_MAX}));
+        // NOTE: replaceWidget() will move focus to another widget, which will screw up the focus order in
+        // LastFocusedFilter. We therefore clear the focus before calling the function, and reapply the focus after.
+        QWidget *regain_focus_widget = nullptr;
+        if (source_widget->isAncestorOf(focusWidget()))
+        {
+            regain_focus_widget = focusWidget();
+            regain_focus_widget->clearFocus();
+        }
 
-    connectWidget(container);
+        m_splitter->replaceWidget(index, container);
+
+        if (regain_focus_widget)
+            regain_focus_widget->setFocus();
+
+        // NOTE: Adding the source_widget to the container after calling replaceWidget() will ensure that the splitter
+        // ratio will be preserved.
+        container->addWidget(source_widget);
+        container->addWidget(new_widget);
+        container->m_splitter->setSizes(QList<int>({INT_MAX, INT_MAX}));
+
+        connectWidget(container);
+    }
 }
